04d1d705f5cc93b8d330782b9794f7bf0c984b53
Eric <breezeforios@gmail.com>
2013-06-22 08:27:15 +0800

    init
    init

---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by Breeze on 13-6-22.
+//  Copyright (c) 2013年 Breeze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestDemo/TestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by Breeze on 13-6-22.
+//  Copyright (c) 2013年 Breeze. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
